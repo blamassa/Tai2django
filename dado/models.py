c054d6b257d52b47a687fff1863ebcc1ecb8544a
Breno Lamassa <brenoelamassa@gmail.com>
2017-10-28 20:50:06 -0200

    Updated tables

--- dado/models.py
+++ dado/models.py
@@ -1,16 +1,31 @@
 from django.db import models
+from django.db.models import F 
 from django.utils import timezone
 
 # Create your models here.
 
 class Dado(models.Model):
-	massa = models.CharField(verbose_name = 'Massa de cafe(g)', max_length = 10)
-	hexano = models.CharField(verbose_name = 'Volume de hexano utilizado', max_length = 10)
-	oleo = models.CharField(verbose_name = 'Volume de oleo obtido(ml)', max_length = 10)
-	data = models.DateTimeField(verbose_name = 'Data da extracao', null = True, default = timezone.now)
+    	
+	massa = models.FloatField(verbose_name = 'Massa de cafe(g)', blank = False)
+	hexano = models.FloatField(verbose_name = 'Volume de hexano utilizado', blank = False)
+	oleo = models.FloatField(verbose_name = 'Volume de oleo obtido(ml)', blank = False)
+	data = models.DateTimeField(verbose_name = 'Data da extracao', max_length = 100)
+
 
 	def __str__(self):
-		return self.massa
+    		return self.massa
+
+	def get_massa(self):
+    		return self.massa
+	
+	def get_hexano(self):
+    		return self.massa
+	
+	def get_oleo(self):
+    		return self.oleo
 
     #def __str__(self):
-    #    return self.massa
+    #    return self.massa	
+	
+#relacao = Dado.objects.all().annotate(value = (84.6 * F('oleo'))/F('massa'))
+	
--- dado/views.py
+++ dado/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from .models import Dado
+import datetime
 
 # Create your views here.
 
 def dado(request):
 	dado = Dado.objects.order_by('data')
+	#oleo = dado.objects.all().oleo
+	#massa = dado.objects.all().massa
+	#relacao = (84.6*oleo)/(massa)
+	#dado.append(relacao)
 	return render(request, 'index.html',{'dado':dado})
